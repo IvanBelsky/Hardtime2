bf46a74f62d1b13ef72b1e65d7e91e436fd02e29
IvanBelsky <mostygas@tut.by>
2022-10-22 00:32:40 +0300

    create entity User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -13,8 +14,218 @@ class User
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 250, nullable: true)]
+    private ?string $тname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $middleName = null;
+
+    #[ORM\Column(length: 250)]
+    private ?string $surname = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateOfBirth = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $phone = null;
+
+    #[ORM\Column(length: 1, nullable: true)]
+    private ?string $gender = null;
+
+    #[ORM\Column]
+    private ?int $registrationStepPassed = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateLastLogin = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateCreated = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateBanned = null;
+
+    #[ORM\Column(length: 1)]
+    private ?string $isActive = null;
+
+    #[ORM\Column(length: 1)]
+    private ?string $isBanned = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $cardId = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getтname(): ?string
+    {
+        return $this->тname;
+    }
+
+    public function setтname(?string $тname): self
+    {
+        $this->тname = $тname;
+
+        return $this;
+    }
+
+    public function getMiddleName(): ?string
+    {
+        return $this->middleName;
+    }
+
+    public function setMiddleName(?string $middleName): self
+    {
+        $this->middleName = $middleName;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDateOfBirth(): ?\DateTimeInterface
+    {
+        return $this->dateOfBirth;
+    }
+
+    public function setDateOfBirth(?\DateTimeInterface $dateOfBirth): self
+    {
+        $this->dateOfBirth = $dateOfBirth;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getRegistrationStepPassed(): ?int
+    {
+        return $this->registrationStepPassed;
+    }
+
+    public function setRegistrationStepPassed(int $registrationStepPassed): self
+    {
+        $this->registrationStepPassed = $registrationStepPassed;
+
+        return $this;
+    }
+
+    public function getDateLastLogin(): ?\DateTimeInterface
+    {
+        return $this->dateLastLogin;
+    }
+
+    public function setDateLastLogin(\DateTimeInterface $dateLastLogin): self
+    {
+        $this->dateLastLogin = $dateLastLogin;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getDateBanned(): ?\DateTimeInterface
+    {
+        return $this->dateBanned;
+    }
+
+    public function setDateBanned(?\DateTimeInterface $dateBanned): self
+    {
+        $this->dateBanned = $dateBanned;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?string
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(string $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getIsBanned(): ?string
+    {
+        return $this->isBanned;
+    }
+
+    public function setIsBanned(string $isBanned): self
+    {
+        $this->isBanned = $isBanned;
+
+        return $this;
+    }
+
+    public function getCardId(): ?string
+    {
+        return $this->cardId;
+    }
+
+    public function setCardId(?string $cardId): self
+    {
+        $this->cardId = $cardId;
+
+        return $this;
     }
 }
